a25bb28734917463640ed0a3d75d61c5ce7927f4
Enes Kutlay <hi@eneskutlay.dev>
2023-04-10 06:41:41 +0300

    Update story page

--- app/stories/questions/[id].js
+++ app/stories/questions/[id].js
@@ -1,53 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-// import { usePathname } from 'expo-router'; Link for going back to the previous page
-import useStoryQuestions from '../../../src/hooks/useStoryQuestions';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-function StoryDetails() {
+import useStoryQuestions from '../../../src/hooks/useStoryQuestions';
+import Story from '../../../src/components/Story';
+
+const StoryDetails = () => {
   const { questions, currentQuestionIndex, handleAnswerClick } = useStoryQuestions();
+
   return (
-    <View style={styles.root}>
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <Text>Maybe Image Here</Text>
+      </View>
       {questions.length > 0 && (
-        <>
-          <Text style={styles.rootText}>{questions[currentQuestionIndex].questionText}</Text>
-        </>
+        <Story question={questions[currentQuestionIndex]} handleAnswerClick={handleAnswerClick} />
       )}
     </View>
   );
-}
-
-/*
-      {questions.length > 0 && (
-        <>
-          <View style={styles.questionSection}>
-            <Text style={styles.questionString}>
-              {questions[currentQuestionIndex].questionText}
-            </Text>
-          </View>
-          <View style={styles.answersSection}>
-            {questions[currentQuestionIndex].answers.map((answer) => (
-              <TouchableOpacity key={answer.answerId} onPress={() => handleAnswerClick(answer)}>
-                <Text style={styles.answerString}>{answer.answerText}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-        </>
-      )}
-
-*/
+};
 
 const styles = StyleSheet.create({
-  root: {
+  container: {
     flex: 1,
+    backgroundColor: '#1E1E1E',
+  },
+  image: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1E1E1E',
-    width: '100%',
-    height: 'auto',
-    color: '#fff',
-  },
-  rootText: {
-    color: '#fff',
+    backgroundColor: 'pink',
+    opacity: 0.2,
+    height: 200,
   },
 });
 
