89d75e9fc5f9225bc622020b14f9dd7ce8c10768
Enes Kutlay <hi@eneskutlay.dev>
2023-04-11 07:32:20 +0300

    Prettier

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-function RootPage() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Root Screen</Text>
-      <Link href='/stories'>Go to Main</Link>
+      <Link href="/stories">Go to Main</Link>
     </View>
   );
 }
@@ -13,10 +13,10 @@ function RootPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#1E1E1E',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1E1E1E",
   },
 });
 
-export default RootPage;
+export default App;
