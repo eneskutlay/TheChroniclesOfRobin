7a6923b6d2cecf77ee926398241670f9bd07694c
Enes Kutlay <hi@eneskutlay.dev>
2023-04-15 18:20:28 +0300

    Index page update

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useRouter } from 'expo-router';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Header, RootButton } from '@components';
 function App() {
+  const router = useRouter();
+  const lorem = ' Welcome the game of choices! ';
+  let objChild = { children: ['Books', 'Options', 'Credits'] };
   return (
     <View style={styles.container}>
-      <Text>Root Screen</Text>
-      <Link href="/books">Go to Main</Link>
+      <View style={styles.mainContent}>
+        <Header children={lorem} />
+      </View>
+      <View style={styles.mainBody}>
+        {objChild.children.map((child, index) => {
+          return (
+            <RootButton press={() => router.push(`${child.toLowerCase()}`)} key={index}>
+              {child}
+            </RootButton>
+          );
+        })}
+      </View>
     </View>
   );
 }
@@ -16,6 +29,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#1E1E1E',
+    width: '100%',
+  },
+  mainContent: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  mainBody: {
+    flex: 2,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
   },
 });
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as LoadingAnimation } from './Animation';
 export { default as Book } from './Book';
-export { default as BackButton } from './Buttons';
+export { BackButton, RootButton } from './Buttons';
 export { default as Content } from './Content';
 export { Header, Description, BookCoverName, IntroText } from './Texts';
